55fc838e07b3706d47c877416bc5309b58fc6ea9
Bruno Cabral <cabral.brc@gmail.com>
2022-02-03 18:59:11 -0800

    create and destroy new blocks

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import ursina as ur
+from ursina.prefabs.first_person_controller import FirstPersonController
 
 class Voxel(ur.Button):
     def __init__(self, position = (0,0,0)):
@@ -10,12 +11,21 @@ class Voxel(ur.Button):
                 texture = 'white_cube',
                 color = ur.color.white,
                 highlight_color = ur.color.lime)
-        pass
+    
+    def input(self, key):
+        if self.hovered:
+            if key == 'left mouse down':
+                # ur.mouse.normal is the surface where mouse is pointing at
+                voxel = Voxel(position= (self.position + ur.mouse.normal))
+            
+            if key == 'right mouse down':
+                ur.destroy(self)
 
 app = ur.Ursina()
 
 for z in range(8):
     for x in range(8):
         voxel = Voxel( position = (x, 0, z) )
+player = FirstPersonController()
 
 app.run()
